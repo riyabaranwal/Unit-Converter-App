2bc7c060ef7fcf0f7a9a86008906927bd0929c72
riyabaranwal <riya.rb203@gmail.com>
2024-06-30 11:11:10 +0530

    Unit-Converter App

--- app/src/main/java/eu/tutorials/unitconverter/MainActivity.kt
+++ app/src/main/java/eu/tutorials/unitconverter/MainActivity.kt
@@ -1,7 +1,6 @@
 package eu.tutorials.unitconverter
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -22,7 +21,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -30,7 +28,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import eu.tutorials.unitconverter.ui.theme.UnitConverterTheme
